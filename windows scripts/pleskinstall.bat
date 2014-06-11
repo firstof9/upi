b34d2f5a128d248898607db07e33dd02ddfd7893
firstof9 <firstof9@gmail.com>
2014-06-10 23:00:32 -0700

    Update pleskinstall.bat

--- windows scripts/pleskinstall.bat
+++ windows scripts/pleskinstall.bat
@@ -6,6 +6,6 @@ Start /w pkgmgr /iu:IIS-WebServerRole;IIS-WebServer;IIS-CommonHttpFeatures;IIS-S
 
 echo %date% %time% >> c:\Tools\pleskinstall.log
 echo "Starting Plesk install" >> c:\Tools\pleskinstall.log
-c:\Tools\parallels_installer_3.15.14.exe --select-release-id PANEL_11_0_9_WIN --install-component base --install-component awstats --install-component mailenable --install-component dns --install-component spamassassin --install-component mysql-client --install-component phpmyadmin --install-component plesk-migration-manager --install-component php53 --install-component atmail --install-component webmail --no-daemon
+c:\Tools\ai.exe --select-release-id PANEL_11_0_9_WIN --install-component base --install-component awstats --install-component mailenable --install-component dns --install-component spamassassin --install-component mysql-client --install-component phpmyadmin --install-component plesk-migration-manager --install-component php53 --install-component atmail --install-component webmail --no-daemon
 echo %date% %time% >> c:\pleskinstall.log
 echo "Finished Plesk install" >> c:\Tools\pleskinstall.log
